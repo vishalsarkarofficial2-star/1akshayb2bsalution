bad239c176205b545019f56256456a91d5b0617f
vishalsarkarofficial2-star <vishalsarkarofficial2@gmail.com>
2026-08-23 12:25:48 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '.1akshayb2bsalution/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
